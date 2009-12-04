0f33343a31fed3b5e2dbfec7d33096dca642c863
karottenreibe <karottenreibe@gmail.com>
2009-12-05 00:03:36 +0100

    added Node remote interface added NodeImpl first draft of implementation added Pair class

--- src/main/java/decentchat/internal/nodes/AbstractNode.java
+++
@@ -1,7 +0,0 @@
-package decentchat.internal.nodes;
-
-import java.io.Serializable;
-
-public abstract class AbstractNode implements Serializable {
-
-}
---
+++ src/main/java/decentchat/internal/nodes/NodeImpl.java
@@ -0,0 +1,50 @@
+package decentchat.internal.nodes;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+
+public class NodeImpl extends UnicastRemoteObject implements Node {
+	
+	private NodeKey key;
+	private int port;
+	private Node predecessor;
+	private List<Node> successors;
+
+	protected NodeImpl() throws RemoteException {
+		super();
+	}
+
+	@Override
+	public Pair<Node, Boolean> getCloserNode(NodeKey wanted) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NodeKey getKey() {
+		return key;
+	}
+
+	@Override
+	public int getPort() {
+		return port;
+	}
+
+	@Override
+	public Node getPredecessor() {
+		return predecessor;
+	}
+
+	@Override
+	public List<Node> getSuccessors() {
+		return successors;
+	}
+
+	@Override
+	public void notify(Node predecessor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

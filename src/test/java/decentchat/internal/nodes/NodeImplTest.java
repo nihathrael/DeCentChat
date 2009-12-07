7cb543ed124e4de17c5661a13c98cc48c718f807
karottenreibe <karottenreibe@gmail.com>
2009-12-07 14:16:07 +0100

    added some tests for NodeImpl and RingMaintainer

---
+++ src/test/java/decentchat/internal/nodes/NodeImplTest.java
@@ -0,0 +1,55 @@
+package decentchat.internal.nodes;
+
+import java.rmi.RemoteException;
+
+import org.junit.*;
+
+import decentchat.internal.NodeKey;
+import static org.junit.Assert.*;
+
+public class NodeImplTest {
+	
+	private NodeImpl setupNode(int key, NodeImpl seed) throws RemoteException {
+		return new NodeImpl(NodeKey.MIN_KEY.inc(key), seed);
+	}
+	
+	private NodeImpl setupNode(int key) throws RemoteException {
+		return new NodeImpl(NodeKey.MIN_KEY.inc(key));
+	}
+	
+	@Test
+	public void testCreate() throws RemoteException {
+		NodeImpl seed_node = setupNode(0);
+		assertNull(seed_node.getPredecessor());
+		assertEquals(seed_node, seed_node.getSuccessor());
+		assertEquals(0, seed_node.getSuccessors().size());
+		assertEquals(0, seed_node.getFingers().size());
+	}
+	
+	@Test
+	public void testJoin() throws RemoteException {
+		NodeImpl eight = setupNode(8);
+		NodeImpl ten = setupNode(10, eight);
+		assertNull(ten.getPredecessor());
+		assertEquals(eight, ten.getSuccessor());
+	}
+	
+	@Test
+	public void testJoinBackwards() throws RemoteException {
+		NodeImpl ten = setupNode(10);
+		NodeImpl eight = setupNode(8, ten);
+		assertNull(eight.getPredecessor());
+		assertEquals(ten, eight.getSuccessor());
+	}
+	
+	@Test
+	public void testNotify() throws RemoteException {
+		NodeImpl ten = setupNode(10);
+		NodeImpl eight = setupNode(8, ten);
+		eight.notify(ten);
+		assertEquals(ten, eight.getPredecessor());
+		ten.notify(eight);
+		assertEquals(eight, ten.getPredecessor());
+	}
+
+}
---
+++ src/test/java/decentchat/internal/nodes/RingMaintainerTest.java
@@ -0,0 +1,50 @@
+package decentchat.internal.nodes;
+
+import java.rmi.RemoteException;
+import decentchat.internal.NodeKey;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class RingMaintainerTest {
+	
+	private NodeImpl[] nodes;
+	private RingMaintainer[] maintainers;
+	
+	private NodeImpl setupNode(int key, NodeImpl seed) throws RemoteException {
+		return new NodeImpl(NodeKey.MIN_KEY.inc(key), seed);
+	}
+	
+	private NodeImpl setupNode(int key) throws RemoteException {
+		return new NodeImpl(NodeKey.MIN_KEY.inc(key));
+	}
+	
+	@Before
+	public void setup() throws RemoteException {
+		int[] node_ids = {1, 8, 14, 21, 32, 38, 42, 48, 51, 56};
+		nodes = new NodeImpl[node_ids.length];
+		nodes[0] = setupNode(node_ids[0]);
+		for (int i = 1; i < node_ids.length; ++i) {
+			nodes[i] = setupNode(node_ids[i], nodes[0]);
+		}
+		maintainers = new RingMaintainer[nodes.length];
+		for (int i = 0; i < nodes.length; ++i) {
+			maintainers[i] = new RingMaintainer(nodes[i]);
+		}
+	}
+	
+	@Test
+	public void testStabilize() {
+		for (int i = 0; i < 10; ++i) {
+			for (int k = 0; k < nodes.length; ++k) {
+				maintainers[k].stabilize();
+			}
+		}
+		
+		for (int i = 1; i < nodes.length; ++i) {
+			assertEquals("Successor of " + nodes[i-1] + "is wrong", nodes[i], nodes[i-1].getSuccessor());
+		}
+	}
+
+}
